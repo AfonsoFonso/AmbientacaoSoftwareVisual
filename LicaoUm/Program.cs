8ec3e7ab1c25baef07d0147461091f758157198e
Bernardo <fofanobernardo0@gmail.com>
2025-08-02 19:03:25 -0300

    Exercício 1
    Exercício para leitura de medidas e cálculo de área de um retângulo.

---
+++ LicaoUm/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Insira a altura do retângulo: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Insira a largura do retângulo: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+double area = altura * largura;
+Console.WriteLine($"A área do retângulo é: " + area);
