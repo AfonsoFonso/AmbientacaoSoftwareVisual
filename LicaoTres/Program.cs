1fc413ee73cfe5e4a23a31585d1b69825a4f66ba
Bernardo <fofanobernardo0@gmail.com>
2025-08-02 19:13:44 -0300

    Exercício Três
    Exercício para ler dois números e comparar qual é maior.

---
+++ LicaoTres/Program.cs
@@ -0,0 +1,17 @@
+﻿// Exercício para leitura de dois numeros e informar qual é o maior e qual é o menor
+Console.WriteLine("Insira o primeiro número: ");
+double primeironumero = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Insira o segundo número: ");
+double segundonumero = Convert.ToDouble(Console.ReadLine());
+if (primeironumero > segundonumero)
+{
+    Console.WriteLine("O maior número é o " + primeironumero + " e o menor é o " + segundonumero);
+}
+else if (segundonumero > primeironumero)
+{
+    Console.WriteLine("O maior número é " + segundonumero + " e o menor é " + primeironumero);
+}
+else
+{
+    Console.WriteLine("Os números são iguais.");
+}
