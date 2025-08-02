38bc70dfc2c3091e387eb2e6728770c267ca5be4
Bernardo <fofanobernardo0@gmail.com>
2025-08-02 19:04:36 -0300

    Exercício 2
    Exercício para leitura de valor em reais e conversão para outras moedas.

---
+++ LicaoDois/Program.cs
@@ -0,0 +1,9 @@
+﻿// ALgoritmo para conversão de real para dólar, euro e peso argentino
+Console.WriteLine("insira o valor em reais para conversão: ");
+double real = Convert.ToDouble(Console.ReadLine());
+double dolar = real*5.17; // taxa de conversão do real para dólar
+double euro = real*6.14; // taxa de conversão do real para euro
+double pesoArgentino = real*0.05; // taxa de conversão do real para peso argentino
+Console.WriteLine("Valor em dólares: $" + dolar);
+Console.WriteLine("Valor em euros: €" + euro);
+Console.WriteLine("Valor em pesos argentinos: $" + pesoArgentino); 
