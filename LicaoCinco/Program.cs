b8d9cb316ea0a615260156b89ba5922ca8b1bb54
Bernardo <fofanobernardo0@gmail.com>
2025-08-02 21:47:56 -0300

    Exercício Cinco
    Exercício de sequência de Fibonacci

---
+++ LicaoCinco/Program.cs
@@ -0,0 +1,30 @@
+﻿        // Exercício Sequencia Fibonacci
+        Console.WriteLine("--- Sequência Fibonacci ---");
+        Console.Write("Digite um valor inteiro e positivo como limite: ");
+
+        if (int.TryParse(Console.ReadLine(), out int limite) && limite >= 0)
+        {
+            int a = 0;
+            int b = 1;
+
+            Console.WriteLine($"\nSequência Fibonacci até {limite}:");
+
+            Console.Write(a);
+
+            while (b <= limite)
+            {
+
+                Console.Write(", " + b);
+
+                int proximo = a + b;
+
+                a = b;
+                b = proximo;
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("\nEntrada inválida. Por favor, execute o programa novamente e digite um número inteiro e positivo.");
+        }
+    
