456f03a50a0694d18238c00297aad6817db2259f
Bernardo <fofanobernardo0@gmail.com>
2025-08-02 21:34:59 -0300

    Exercício 4
    Exercício para identificação de faixa etária.

---
+++ LicaoQuatro/Program.cs
@@ -0,0 +1,20 @@
+﻿// Exercício para identificação de faixa etária
+Console.WriteLine("Informe sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+if (idade <= 13)
+{
+    Console.WriteLine("Você é uma criança.");
+}
+else if (idade <= 18)
+{
+    Console.WriteLine("Você é um Adolescente.");
+}
+else if (idade <= 60)
+{
+    Console.WriteLine("Você é um Adulto.");
+}
+else
+{
+    Console.WriteLine("Você é um Idoso.");
+}
+
